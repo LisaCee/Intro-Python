e058db2807485bec203be51d3bba7d2a2b3469da
Lisa Canini <three_nineteen@yahoo.com>
2018-08-01 11:40:01 -0700

    cal wip

--- src/day-1-toy/cal.py
+++ src/day-1-toy/cal.py
@@ -14,3 +14,12 @@
 # docs for the calendar module closely.
 
 import sys
+import calendar
+import datetime
+
+month_year = input ("Enter a month and year: ")
+# print(month_year)
+# # itermonthdates(month_year[0], month_year[1])
+# str = calendar.monthcalendar(1999, 1)
+# print(str)
+print(sys.argv)
