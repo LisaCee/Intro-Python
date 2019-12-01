cd4e94c52a56e2b7aa4ed07632d6770ebab902f6
LisaCee <lisaceedesign@gmail.com>
2019-12-01 15:34:29 -0800

    args

--- src/11_args.py
+++ src/11_args.py
@@ -6,12 +6,25 @@
 
 # YOUR CODE HERE
 
+
+def f1(x, y):
+    return x + y
+
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+
+def f2(*args):
+    total = 0
+    for num in args:
+        total = total + num
+    return total
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,6 +34,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
+a = 22
 print(f2(a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
@@ -28,6 +42,14 @@ print(f2(a))    # Should print 22
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+
+
+def f3(x, y=0):
+    if x == x + y:
+        return x + 1
+    else:
+        return x + y
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,16 +64,20 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print("key: {}, value: {}".format(key, value))
+
 
 # Should print
 # key: a, value: 12
 # key: b, value: 30
 f4(a=12, b=30)
 
-# Should print
-# key: city, value: Berkeley
-# key: population, value: 121240
-# key: founded, value: "March 23, 1868"
+# # Should print
+# # key: city, value: Berkeley
+# # key: population, value: 121240
+# # key: founded, value: "March 23, 1868"
 f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
 d = {
@@ -59,5 +85,5 @@ d = {
     "hp": 3
 }
 
-# What thing do you have to add to make this work?
-f4(d)
+# # What thing do you have to add to make this work?
+f4(d=d)
